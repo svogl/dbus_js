090635402dc141629a573d6f9a23915897a8fd1c
Simon Vogl <simon.vogl@beko.at>
2010-03-03 12:48:30 +0100

    added mainloop quit function to static glib object.

--- src/glib_js.cpp
+++ src/glib_js.cpp
@@ -20,7 +20,7 @@
 using namespace std;
 
 
-GMainLoop* mainloop = NULL;
+    GMainLoop* mainloop = NULL;
 
 
 ////////////////////////////////////////
@@ -28,118 +28,114 @@ GMainLoop* mainloop = NULL;
 ////////////////////////////////////////
 
 typedef struct _Glib {
-	// my data;
-	char* name;	    ///< bus name 
-
-	JSObject* self;     ///< the javascript object
-	JSContext*  cx;     ///< the javascript context
+    // my data;
+    char* name; ///< bus name
 
 } glibData;
 
 
 /*************************************************************************************************/
 /** Glib Constructor */
-static JSBool GlibConstructor(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) 
-{
-	int i;
-	if (argc == 0) {
-		glibData* dta = NULL;
+static JSBool GlibConstructor(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+    int i;
+    if (argc == 0) {
+        glibData* dta = NULL;
 
-		dta = (glibData*)calloc(sizeof(glibData), 1);
-		
-		if (!JS_SetPrivate(cx, obj, dta)) 
-			return JS_FALSE;
-		return JS_TRUE;
-	}
-	return JS_FALSE;
+        dta = (glibData*) calloc(sizeof (glibData), 1);
+
+        if (!JS_SetPrivate(cx, obj, dta))
+            return JS_FALSE;
+        return JS_TRUE;
+    }
+    return JS_FALSE;
 }
 
 static void GlibDestructor(JSContext *cx, JSObject *obj) {
-	printf("Destroying Glib object\n");
-	glibData* dta = (glibData *)JS_GetPrivate(cx, obj);
-	if (dta) {
-		free(dta->name);
-		free(dta);
-	}
+    printf("Destroying Glib object\n");
+    glibData* dta = (glibData *) JS_GetPrivate(cx, obj);
+    if (dta) {
+        free(dta->name);
+        free(dta);
+    }
 }
 
 /*************************************************************************************************/
 
 ///// Glib Function Table
 static JSFunctionSpec _GlibFunctionSpec[] = {
-//	{ "connectToSignal",    Glib_connectToSignal,   0, 0, 0 },
-//	{ "emitSignal",          Glib_emitSignal,    	0, 0, 0 },
-/*
-	{ "position",          Actor_set_position,    0, 0, 0 },
-	{ "size",              Actor_set_size,    0, 0, 0 },
-	{ "addActor",          Actor_container_add,    0, 0, 0 },
-	{ "showAll",           Actor_show_all,    0, 0, 0 },
-	{ "connect",           Actor_connect,    0, 0, 0 },
-	{ "gtk_connect",       Actor_gtk_connect,    0, 0, 0 },
-*/
-	{ 0, 0, 0, 0, 0 }
+    { 0, 0, 0, 0, 0}
 };
 
 /*************************************************************************************************/
 ///// Glib Class Definition
 
 static JSClass Glib_jsClass = {
-	"Glib", JSCLASS_HAS_PRIVATE,
-	JS_PropertyStub, JS_PropertyStub, // add/del prop
-	JS_PropertyStub, JS_PropertyStub, // get/set prop
-	JS_EnumerateStub, JS_ResolveStub, // enum / resolve
-	JS_ConvertStub, GlibDestructor,  // convert / finalize
-	0, 0, 0, 0,                  
-	0, 0, 0, 0
+    "Glib", JSCLASS_HAS_PRIVATE,
+    JS_PropertyStub, JS_PropertyStub, // add/del prop
+    JS_PropertyStub, JS_PropertyStub, // get/set prop
+    JS_EnumerateStub, JS_ResolveStub, // enum / resolve
+    JS_ConvertStub, GlibDestructor, // convert / finalize
+    0, 0, 0, 0,
+    0, 0, 0, 0
 };
 /*************************************************************************************************/
+
 /* static funcs: */
-static JSBool Glib_s_mainloop(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
-{
-	printf("object is in mainloop: %p\n",obj);
+static JSBool Glib_s_mainloop(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+    printf("object is in mainloop: %p\n", obj);
 
-	check_args( (argc == 0), "must not pass an argument!\n");
+    check_args((argc == 0), "must not pass an argument!\n");
+    /*
+    JSObject *bus = JS_ConstructObject(cx, &Glib_jsClass, NULL, NULL);
+    glibData* dta = (glibData *) JS_GetPrivate(cx, bus);
+    */
+    g_main_run(mainloop);
 
-	JSObject *bus = JS_ConstructObject(cx, &Glib_jsClass, NULL,NULL);
-	glibData* dta = (glibData *)JS_GetPrivate(cx, bus);
+    return JS_TRUE;
+}
 
-        g_main_run(mainloop);
+static JSBool Glib_s_quit(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+    printf("object is in mainloop: %p\n", obj);
 
-	*rval = OBJECT_TO_JSVAL(bus);
-	return JS_TRUE;
+    check_args((argc == 0), "must not pass an argument!\n");
+
+    g_main_loop_quit(mainloop);
+
+    return JS_TRUE;
 }
 
 static JSFunctionSpec _GlibStaticFunctionSpec[] = {
-	{ "mainloop",   Glib_s_mainloop,          0, 0, 0 },
-	{ 0, 0, 0, 0, 0 }
+    { "mainloop", Glib_s_mainloop, 0, 0, 0},
+    { "quit", Glib_s_quit, 0, 0, 0},
+    { 0, 0, 0, 0, 0}
 };
 
 
 ///// Actor Initialization Method
+
 JSObject* GlibInit(JSContext *cx, JSObject *obj) {
-	if (obj==NULL)
-		obj = JS_GetGlobalObject(cx);
+    if (obj == NULL)
+        obj = JS_GetGlobalObject(cx);
 
-	// Clutter
-	JS_InitClass(cx, obj, NULL, 
-			&Glib_jsClass, 
-			GlibConstructor, 0, 
-			NULL, // properties 
-			 _GlibFunctionSpec, // functions
-    	                NULL, // static properties
-			_GlibStaticFunctionSpec // static functions
-			);
+    JS_InitClass(cx, obj, NULL,
+            &Glib_jsClass,
+            GlibConstructor, 0,
+            NULL, // properties
+            _GlibFunctionSpec, // functions
+            NULL, // static properties
+            _GlibStaticFunctionSpec // static functions
+            );
 
-	/***************************** glib main loop omot */
-  /* Will store the result of the RequestName RPC here. */
+    /***************************** glib main loop omot */
+    /* Will store the result of the RequestName RPC here. */
 
-  /* Create a main loop that will dispatch callbacks. */
-  mainloop = g_main_loop_new(NULL, FALSE);
-  if (mainloop == NULL) {
-    /* Print error and terminate. */
-    fprintf(stderr, "Could not create GMainLoop!");
-	exit(1);
-  }
+    /* Create a main loop that will dispatch callbacks. */
+    mainloop = g_main_loop_new(NULL, FALSE);
+    if (mainloop == NULL) {
+        /* Print error and terminate. */
+        fprintf(stderr, "Could not create GMainLoop!");
+        exit(1);
+    }
 }
 
 /*************************************************************************************************/
