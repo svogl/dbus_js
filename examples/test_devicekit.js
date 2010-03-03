cfff0dfe7041e7239d06485c82ec076b0d91cdb9
Simon Vogl <svogl@M.(none)>
2010-03-03 12:21:49 +0100

    devicekit test - waits for added/removed drives

---
+++ examples/test_devicekit.js
@@ -0,0 +1,77 @@
+/* This is a simple example that listens for events from the disk subsystem.
+   
+ */
+DSO.load("dbus"); 
+
+
+bus = DBus.systemBus();
+
+function Disk(bus, oPath) {
+	this.bus = bus;
+	this.oPath = oPath;
+}
+
+
+function DeviceKit(bus) {
+	this.bus = bus;
+	this.service = "org.freedesktop.DeviceKit.Disks";
+	this.iFace = "org.freedesktop.DeviceKit.Disks";
+	this.oPath = "/org/freedesktop/DeviceKit/Disks";
+
+	this.disks = {};
+
+	this.devAdd = function(opath) {
+		print("added device " + opath);
+		disks[opath] = Disk(this.bus, opath);
+	};
+
+	this.devRem = function () {
+		print("removed device " + opath);
+	},
+
+	this.devChg = function(opath) {
+		print("changed device " + opath);
+	}
+	this.enumDevices = function() {
+		ret = this.bus.call( this.service, this.oPath, this.iFace, "EnumerateDevices");
+		return ret;
+	}
+
+	this.enumDeviceFiles = function() {
+		ret = this.bus.call( this.service, this.oPath, this.iFace, "EnumerateDeviceFiles");
+		return ret;
+	}
+	print(bus);
+
+	// register signal handlers:
+	val = this.bus.addSigHandler( this.service, this.oPath, this.iFace, "DeviceAdded", this.devAdd);
+	val = this.bus.addSigHandler( this.service, this.oPath, this.iFace, "DeviceRemoved", this.devRem);
+	val = this.bus.addSigHandler( this.service, this.oPath, this.iFace, "DeviceChanged", this.devChg);
+};
+
+
+
+devKit = new DeviceKit(bus);
+
+for (var i in devKit)
+{
+	print(">>> "+ i);
+}
+
+
+print("---\n\n");
+for(var i=0; i < devKit.length; i++ ){
+	print("-> " + devKit[i] );
+}
+print("---\n\n");
+
+print("--- EnumerateDevices\n\n");
+ret = devKit.enumDevices();
+for(var i=0; i < ret.length; i++ ){
+	print("-> " + ret[i] );
+}
+
+Glib.mainloop();
+
+print("------- done");
+
