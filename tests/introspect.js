0139a69a0ec1960760d300bc9bac8594c7a4d4af
Simon Vogl <simon.vogl@beko.at>
2010-04-19 12:30:26 +0200

    added quit function to terminate glib mainloop. test with: dbus-send --session --print-reply --type=method_call --dest=at.beko.inspect /  at.beko.Glib.quit

--- tests/introspect.js
+++ tests/introspect.js
@@ -21,6 +21,17 @@ function Hello() {
 	}
 }
 
+function Quit() {
+	this._iface = "at.beko.Glib";
+	this._sig =   <interface name={this._iface}>
+						<method name="quit">
+						</method>
+				  </interface>;
+	this.quit = function() {
+		Glib.quit();
+	}
+}
+
 
 function DirInfo() {
 	this.name=""; 
@@ -98,179 +109,18 @@ bus._intro = intro;
 intro.expose("/", intro);
 bus.export("/", intro._iface, intro); 
 
-
 hello = new Hello();
 intro.expose("/", hello);
 
-//intro.test();
-//bus._intro.test();
+quit = new Quit();
+intro.expose("/", quit);
+
 print(bus._intro.Introspect());
 
 bus.requestBusName("at.beko.inspect");
 
 Glib.mainloop();
 
-print(intro._sig);
 
-s1=<node>
-  <interface name="org.freedesktop.DBus.Introspectable">
-    <method name="Introspect">
-      <arg name="xml_data" type="s" direction="out"/>
-    </method>
-  </interface>
-  <node name="com"/>
-</node>;
-
-print("***************s1\n"+s1);
-
-/*
-
-
-
-
-
-method call sender=:1.94 -> dest=com.Skype.API serial=144 path=/; interface=org.freedesktop.DBus.Introspectable; member=Introspect
-method return sender=:1.64 -> dest=:1.94 reply_serial=144
-   string "<!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
-"http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
-<node>
-  <interface name="org.freedesktop.DBus.Introspectable">
-    <method name="Introspect">
-      <arg name="xml_data" type="s" direction="out"/>
-    </method>
-  </interface>
-  <node name="com"/>
-</node>
-"
-method call sender=:1.94 -> dest=com.Skype.API serial=145 path=/; interface=org.freedesktop.DBus.Introspectable; member=Introspect
-method return sender=:1.64 -> dest=:1.94 reply_serial=145
-   string "<!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
-"http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
-<node>
-  <interface name="org.freedesktop.DBus.Introspectable">
-    <method name="Introspect">
-      <arg name="xml_data" type="s" direction="out"/>
-    </method>
-  </interface>
-  <node name="com"/>
-</node>
-"
-method call sender=:1.94 -> dest=com.Skype.API serial=146 path=/com; interface=org.freedesktop.DBus.Introspectable; member=Introspect
-method return sender=:1.64 -> dest=:1.94 reply_serial=146
-   string "<!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
-"http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
-<node>
-  <interface name="org.freedesktop.DBus.Introspectable">
-    <method name="Introspect">
-      <arg name="xml_data" type="s" direction="out"/>
-    </method>
-  </interface>
-  <node name="Skype"/>
-</node>
-"
-method call sender=:1.94 -> dest=com.Skype.API serial=147 path=/com; interface=org.freedesktop.DBus.Introspectable; member=Introspect
-method return sender=:1.64 -> dest=:1.94 reply_serial=147
-   string "<!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
-"http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
-<node>
-  <interface name="org.freedesktop.DBus.Introspectable">
-    <method name="Introspect">
-      <arg name="xml_data" type="s" direction="out"/>
-    </method>
-  </interface>
-  <node name="Skype"/>
-</node>
-"
-method call sender=:1.94 -> dest=com.Skype.API serial=148 path=/com/Skype; interface=org.freedesktop.DBus.Introspectable; member=Introspect
-method return sender=:1.64 -> dest=:1.94 reply_serial=148
-   string "<!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
-"http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
-<node>
-  <interface name="com.Skype.API">
-    <method name="Invoke">
-      <arg type="s" direction="out"/>
-      <arg name="request" type="s" direction="in"/>
-    </method>
-  </interface>
-  <interface name="org.freedesktop.DBus.Properties">
-    <method name="Get">
-      <arg name="interface_name" type="s" direction="in"/>
-      <arg name="property_name" type="s" direction="in"/>
-      <arg name="value" type="v" direction="out"/>
-    </method>
-    <method name="Set">
-      <arg name="interface_name" type="s" direction="in"/>
-      <arg name="property_name" type="s" direction="in"/>
-      <arg name="value" type="v" direction="in"/>
-    </method>
-    <method name="GetAll">
-      <arg name="interface_name" type="s" direction="in"/>
-      <arg name="values" type="a{sv}" direction="out"/>
-      <annotation name="com.trolltech.QtDBus.QtTypeName.Out0" value="QVariantMap"/>    </method>
-  </interface>
-  <interface name="org.freedesktop.DBus.Introspectable">
-    <method name="Introspect">
-      <arg name="xml_data" type="s" direction="out"/>
-    </method>
-  </interface>
-</node>
-"
-method call sender=:1.94 -> dest=com.Skype.API serial=149 path=/com/Skype; interface=org.freedesktop.DBus.Introspectable; member=Introspect
-method return sender=:1.64 -> dest=:1.94 reply_serial=149
-   string "<!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
-"http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
-<node>
-  <interface name="com.Skype.API">
-    <method name="Invoke">
-      <arg type="s" direction="out"/>
-      <arg name="request" type="s" direction="in"/>
-    </method>
-  </interface>
-  <interface name="org.freedesktop.DBus.Properties">
-    <method name="Get">
-      <arg name="interface_name" type="s" direction="in"/>
-      <arg name="property_name" type="s" direction="in"/>
-      <arg name="value" type="v" direction="out"/>
-    </method>
-    <method name="Set">
-      <arg name="interface_name" type="s" direction="in"/>
-      <arg name="property_name" type="s" direction="in"/>
-      <arg name="value" type="v" direction="in"/>
-    </method>
-    <method name="GetAll">
-      <arg name="interface_name" type="s" direction="in"/>
-      <arg name="values" type="a{sv}" direction="out"/>
-      <annotation name="com.trolltech.QtDBus.QtTypeName.Out0" value="QVariantMap"/>    </method>
-  </interface>
-  <interface name="org.freedesktop.DBus.Introspectable">
-    <method name="Introspect">
-      <arg name="xml_data" type="s" direction="out"/>
-    </method>
-  </interface>
-</node>
-"
-signal sender=org.freedesktop.DBus -> dest=(null destination) serial=8 path=/org/freedesktop/DBus; interface=org.freedesktop.DBus; member=NameOwnerChanged
-   string ":1.93"
-   string ":1.93"
-   string ""
-signal sender=org.freedesktop.DBus -> dest=(null destination) serial=9 path=/org/freedesktop/DBus; interface=org.freedesktop.DBus; member=NameOwnerChanged
-   string ":1.94"
-   string ":1.94"
-   string ""
-signal sender=org.freedesktop.DBus -> dest=(null destination) serial=10 path=/org/freedesktop/DBus; interface=org.freedesktop.DBus; member=NameOwnerChanged
-   string "org.freedesktop.ReserveDevice1.Audio0"
-   string ""
-   string ":1.0"
-method call sender=:1.0 -> dest=org.freedesktop.DBus serial=27 path=/org/freedesktop/DBus; interface=org.freedesktop.DBus; member=RequestName
-   string "org.freedesktop.ReserveDevice1.Audio0"
-   uint32 5
-signal sender=org.freedesktop.DBus -> dest=(null destination) serial=11 path=/org/freedesktop/DBus; interface=org.freedesktop.DBus; member=NameOwnerChanged
-   string "org.freedesktop.ReserveDevice1.Audio0"
-   string ":1.0"
-   string ""
-method call sender=:1.0 -> dest=org.freedesktop.DBus serial=28 path=/org/freedesktop/DBus; interface=org.freedesktop.DBus; member=ReleaseName
-   string "org.freedesktop.ReserveDevice1.Audio0"
-
-*/
 
 
